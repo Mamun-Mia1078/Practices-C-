0f3e4176986fe998c6069d64c42f12f5377414cf
Mamun Mia <70141554+Mamun-Mia1078@users.noreply.github.com>
2022-06-10 02:06:17 +0600

    practices

---
+++ Practices-C++/inputt.cpp
@@ -0,0 +1,14 @@
+
+
+ #include<iostream>
+ #include<conio.h>
+ using namespace std;
+ int main()
+ {
+    char name;
+    int  num;
+
+    cin>>name >> num;
+     getch();
+ }
+
---
+++ Practices-C++/type.cpp
@@ -0,0 +1,10 @@
+
+ #include<iostream>
+ #include<conio.h>
+ using namespace std;
+ int main()
+ {
+     int num1 = 22, num2 = 34;
+     cout << "Number one value is= " <<num1 <<endl << "Number two value is = " << num2 <<endl ;
+     getch();
+ }
